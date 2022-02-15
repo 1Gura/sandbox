eb02c4e8b872db7c9bf003b5b678d9b423de374c
i.gura <gura.ilya2011@yandex.ru>
2022-02-15 21:56:56 +0300

    Доделываю CRUD

--- src/app/crud/crud.module.ts
+++ src/app/crud/crud.module.ts
@@ -2,22 +2,30 @@ import {NgModule} from "@angular/core";
 import {CrudComponent} from "./crud.component";
 import {CrudRoutingModule} from "./crud.routing.module";
 import {AppService} from "../shared/services/app.service";
-import {MatTabsModule} from "@angular/material/tabs";
 import {CrudService} from "./shared/services/crud.service";
-import {MatTableModule} from '@angular/material/table';
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {BrowserModule} from "@angular/platform-browser";
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+
 import {HttpClientModule} from "@angular/common/http";
+import {TabComponent} from "./tab/tab.component";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {MatNativeDateModule} from "@angular/material/core";
+import {MaterialModule} from "../shared/modules/material.module";
+import {BrowserModule} from "@angular/platform-browser";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
     CrudComponent,
+    TabComponent
   ],
   imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
     CrudRoutingModule,
     HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatNativeDateModule,
+    MaterialModule
   ],
   exports: [CrudComponent],
   providers: [AppService, CrudService],
---
+++ src/app/crud/tab/tab.component.ts
@@ -0,0 +1,15 @@
+import {Component} from '@angular/core';
+
+/**
+ * @title Basic use of the tab group
+ */
+@Component({
+  selector: 'app-tab',
+  templateUrl: 'tab.component.html',
+})
+export class TabComponent {}
+
+
+/**  Copyright 2022 Google LLC. All Rights Reserved.
+ Use of this source code is governed by an MIT-style license that
+ can be found in the LICENSE file at https://angular.io/license */
